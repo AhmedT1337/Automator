e42dc83f40bebf679ae3f33924d4d63b9f400b60
Ahmed Tareq <63518121+AhmedT1337@users.noreply.github.com>
2022-08-15 15:02:57 +0200

    Add files via upload

---
+++ automator.py
@@ -0,0 +1,49 @@
+import requests
+from sys import argv
+from art import tprint
+
+tprint(";-AUTOMATOR-;")
+
+print("           Coded by: Ahmed Tareq\n\n")
+
+if len(argv) == 1 :
+
+    print("Please provide the required input file_name and dir_name or file name")
+
+elif "help" in argv :
+    
+    print("USAGE: python3 automator.py file_name dir_name")
+    
+elif len(argv) == 2 :
+
+    print("Please provide the dir_name or file name")
+
+else :
+
+    with open(argv[1], "r") as f:
+
+        urls = f.read().splitlines()
+    
+    directory = str(argv[2])
+    
+    print(f"looking for {directory} in the provided domains")
+    
+    for item in urls :
+
+        url = item + directory
+
+        try :
+
+            status = requests.get(url, timeout = 1)
+
+            if status.status_code == 200 :
+
+            	print(url)
+
+            else :
+
+                continue
+
+        except :
+
+            pass
